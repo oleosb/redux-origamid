13c2489bbbd4a5791674eeb87736ed655efb7253
Leonardo Barbosa <oleosbo@gmail.com>
2023-08-17 20:37:20 -0300

    Currying

---
+++ middleware/currying/script.js
@@ -0,0 +1,17 @@
+const li = Array.from(document.querySelectorAll("li"));
+
+// const getElementAttr = (key) => {
+//   return function (el) {
+//     return el.getAttribute(key);
+//   };
+// };
+
+const getElementAttr = (key) => (el) => el.getAttribute(key);
+
+const getAttrDataSlide = getElementAttr("data-slide");
+const getAttrId = getElementAttr("id");
+
+const dataSlide = li.map(getAttrDataSlide);
+const idList = li.map(getAttrId);
+
+console.log(dataSlide, idList);
