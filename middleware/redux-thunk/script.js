bd9d50acd208df5d0d4d1c4ff5f14a04db8dcbd8
Leonardo Barbosa <oleosbo@gmail.com>
2023-08-17 22:00:48 -0300

    LocalStorage

--- middleware/redux-thunk/script.js
+++ middleware/redux-thunk/script.js
@@ -1,6 +1,14 @@
+function getLocalStore(key, initial) {
+  try {
+    return JSON.parse(window.localStorage.getItem(key));
+  } catch (error) {
+    return initial;
+  }
+}
+
 const initialState = {
   loading: false,
-  data: null,
+  data: getLocalStore("data", null),
   error: null,
 };
 
@@ -28,11 +36,24 @@ const thunk = (store) => (next) => (action) => {
   return next(action);
 };
 
+const localStorage = (store) => (next) => (action) => {
+  const result = next(action);
+
+  if (action.localStorage !== undefined) {
+    window.localStorage.setItem(
+      action.localStorage,
+      JSON.stringify(action.payload)
+    );
+  }
+
+  return result;
+};
+
 const { applyMiddleware, compose } = Redux;
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const enhancer = composeEnhancers(applyMiddleware(thunk));
+const enhancer = composeEnhancers(applyMiddleware(thunk, localStorage));
 
 const store = Redux.createStore(reducer, enhancer);
 
@@ -41,12 +62,14 @@ function fetchUrl(url) {
     try {
       dispatch({ type: "FETCH_STARTED" });
       const data = await fetch(url).then((r) => r.json());
-      dispatch({ type: "FETCH_SUCCESS", payload: data });
-      console.log(data);
+      dispatch({ type: "FETCH_SUCCESS", payload: data, localStorage: "data" });
     } catch (error) {
       dispatch({ type: "FETCH_ERROR", payload: error.message });
     }
   };
 }
 
-store.dispatch(fetchUrl("https://dogsapi.origamid.dev/json/api/photo"));
+const state = store.getState();
+if (state.data === null) {
+  store.dispatch(fetchUrl("https://dogsapi.origamid.dev/json/api/photo"));
+}
