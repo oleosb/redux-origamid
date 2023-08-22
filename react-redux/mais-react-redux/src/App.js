8a0dc79ea27d25b6c771932c260793e1ca2fcff2
oleosb <oleosbo@gmail.com>
2023-08-22 20:42:05 -0300

    Cache

--- react-redux/mais-react-redux/src/App.js
+++ react-redux/mais-react-redux/src/App.js
@@ -1,40 +1,14 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { adicionarDatas } from "./store/date";
+import React from 'react';
+import Photos from './Photos';
 
 function App() {
-  const [partida, setPartida] = React.useState("");
-  const [retorno, setRetorno] = React.useState("");
-
-  const dispatch = useDispatch();
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    dispatch(adicionarDatas({ partida, retorno }));
-  }
+  const [toggle, setToggle] = React.useState(true);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <p>
-        <label htmlFor="partida">Partida</label>
-        <input
-          type="date"
-          id="partida"
-          onChange={({ target }) => setPartida(target.value)}
-        />
-      </p>
-      <p>
-        <label htmlFor="retorno">Retorno</label>
-
-        <input
-          type="date"
-          id="retorno"
-          onChange={({ target }) => setRetorno(target.value)}
-        />
-      </p>
-      <button>Buscar</button>
-    </form>
+    <div>
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      {toggle && <Photos />}
+    </div>
   );
 }
 
